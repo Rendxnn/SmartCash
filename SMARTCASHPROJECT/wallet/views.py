90fcda079be496bf9a2a3014235e3fbec346f959
Samuel Rendon <samiretru@gmail.com>
2023-02-15 23:55:33 -0500

    second commit

--- SMARTCASHPROJECT/wallet/views.py
+++ SMARTCASHPROJECT/wallet/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Home Page</h1>')
+    return render(request, 'home.html', {'name': 'Samuel Rendon'})
 
 
 def about(request):
