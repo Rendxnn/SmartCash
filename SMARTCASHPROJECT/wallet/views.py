d99d3b5c0f3593dacf77951b28a812e744db3f7d
somendietam <somema501@gmail.com>
2023-03-14 22:30:17 -0500

    Mejoras visuales en la pagina de inicio

--- SMARTCASHPROJECT/wallet/views.py
+++ SMARTCASHPROJECT/wallet/views.py
@@ -12,6 +12,7 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
+            
             form.save()
             username = form.cleaned_data['username']
             # messages.success(request, f"Te has registrado correctamente{username}")
